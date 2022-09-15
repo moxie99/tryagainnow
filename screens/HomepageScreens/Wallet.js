16cec5ac61955255c8f27739ec9753059e41ddf1
Oluwasegun <adeolusegun99@gmail.com>
2022-09-15 21:25:19 +0100

    improved the wallet screen

--- screens/HomepageScreens/Wallet.js
+++ screens/HomepageScreens/Wallet.js
@@ -5,23 +5,33 @@ import {
   TextInput,
   View,
   Switch,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { createMaterialTopTabNavigator } from "react-navigation-tabs";
 import MyTabs from "../../nav/TopTabNav";
+import { FONTS } from "../../constant";
+import { FontAwesome5 } from "@expo/vector-icons";
+import LinearGradient from "react-native-linear-gradient";
 
 const Wallet = () => {
   const [isEnabled, setIsEnabled] = React.useState(false);
+  const [isActive, setIsActive] = React.useState(true);
+  const [toggle, setToggle] = React.useState(false);
+  const [toggleState, setToggleState] = React.useState(1);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#0D3602" }}>
       <View
         style={{
           flexDirection: "row",
           alignItems: "center",
           justifyContent: "space-between",
-          paddingVertical: "20%",
+          paddingVertical: "10%",
           paddingHorizontal: "10%",
         }}
       >
@@ -30,7 +40,7 @@ const Wallet = () => {
           <TextInput
             style={{
               backgroundColor: "white",
-              width: "80%",
+              width: "70%",
               height: 50,
               color: "black",
               borderRadius: 15,
@@ -46,32 +56,189 @@ const Wallet = () => {
             onValueChange={toggleSwitch}
             value={isEnabled}
           />
-          <Text>Hide Balance</Text>
+          <Text style={{ fontFamily: FONTS.light, color: "white" }}>
+            Hide Balance
+          </Text>
         </View>
       </View>
-      <View>
-        <Text>Wallet</Text>
+      <View style={{ paddingHorizontal: "5%" }}>
+        <Text
+          style={{ fontFamily: FONTS.semiBold, fontSize: 30, color: "white" }}
+        >
+          Wallet
+        </Text>
       </View>
-      {/* <View
+      <View
         style={{
           width: "90%",
           height: 150,
-          backgroundColor: "green",
-          alignItems: "center",
+          backgroundColor: "#024002",
           paddingHorizontal: "10%",
           justifyContent: "center",
+          marginHorizontal: "5%",
+          marginTop: "5%",
         }}
       >
-        <Text>Current Balance</Text>
-        <Text>1.00965</Text>
-        <Text>iBOOLA</Text>
-        <Text>147%</Text>
-        <Ionicons name="arrow-up" size={24} color="black" />
-        <Text>Load</Text>
-        <Text>Recharge</Text>
-        <Text>Load</Text>
-      </View> */}
-      <MyTabs />
+        <Text
+          style={{ fontSize: 15, fontFamily: FONTS.regular, color: "white" }}
+        >
+          Current Balance
+        </Text>
+        <Text style={{ fontSize: 20, fontFamily: FONTS.bold, color: "white" }}>
+          1.00965 {"   "}
+          <Text>iBOOLA</Text>
+        </Text>
+        <Text
+          style={{ fontSize: 20, fontFamily: FONTS.bold, color: "#07E607" }}
+        >
+          147% <Ionicons name="arrow-up" size={14} color="#07E607" />
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            paddingVertical: "5%",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              width: "25%",
+              height: 30,
+              backgroundColor: "#06BF06",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: FONTS.semiBold,
+                color: "white",
+                fontSize: 14,
+              }}
+            >
+              Load
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: "25%",
+              height: 30,
+              backgroundColor: "#06BF06",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: FONTS.semiBold,
+                color: "white",
+                fontSize: 14,
+              }}
+            >
+              Exchange
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: "25%",
+              height: 30,
+              backgroundColor: "#06BF06",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: FONTS.semiBold,
+                color: "white",
+                fontSize: 14,
+              }}
+            >
+              Withdraw
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          paddingVertical: "5%",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            borderColor: isActive ? "white" : null,
+            borderBottomWidth: 1,
+          }}
+        >
+          <Text
+            style={{
+              color: isActive ? "white" : "lightgray",
+              fontSize: 16,
+              fontFamily: FONTS.semiBold,
+            }}
+          >
+            Your Activities
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderColor: toggle ? "white" : null,
+            borderBottomWidth: 1,
+          }}
+        >
+          <Text
+            style={{
+              color: toggle ? "white" : "#858181",
+              borderBottomWidth: 2,
+              fontSize: 16,
+              fontFamily: FONTS.semiBold,
+            }}
+          >
+            Details
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {isActive && (
+        <>
+          <View
+            style={{
+              width: "80%",
+              height: 300,
+              backgroundColor: "#e3dede",
+              marginLeft: "10%",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <View
+              style={{
+                width: 150,
+                height: 150,
+                borderRadius: "70%",
+                backgroundColor: "green",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <FontAwesome5
+                name="clipboard-list"
+                size={130}
+                color="white"
+                style={{ position: "absolute", top: 40 }}
+              />
+            </View>
+          </View>
+        </>
+      )}
     </SafeAreaView>
   );
 };
